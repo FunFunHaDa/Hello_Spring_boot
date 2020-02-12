f21769f26db13a2d35a5e82b403df7bfaf8474eb
funfunhada <kjhkjh0929@gmail.com>
2020-02-13 02:38:53 +0900

    3-3  HTML 적용 및 URL 리팩토링

--- src/main/java/com/hello/web/UserController.java
+++ src/main/java/com/hello/web/UserController.java
@@ -5,30 +5,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.hello.domain.User;
 import com.hello.domain.UserReposity;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 	
 	@Autowired
 	private UserReposity userReposity;
 	
-	@PostMapping("/create")
+	@PostMapping("")
 	public String create(User user)
 	{
 		System.out.println(user);
 		userReposity.save(user);
-		return "redirect:/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("")
 	public String list(Model model)
 	{
 		System.out.println("model: "+ model);
 		model.addAttribute("users", userReposity.findAll());
-		return "list";
+		return "/user/list";
 	}
 
 }
