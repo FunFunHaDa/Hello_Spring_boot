eb89f2976712ed798104205ce435c9986bf226b5
funfunhada <kjhkjh0929@gmail.com>
2020-02-16 20:52:30 +0900

    post 형태 수정 기능

--- src/main/java/com/hello/domain/User.java
+++ src/main/java/com/hello/domain/User.java
@@ -10,67 +10,60 @@ public class User {
 	@Id
 	@GeneratedValue
 	private Long id;
-	
+
 	@Column(nullable = false, length = 20)
 	private String UserID;
-	
+
 	private String UserName;
 	private String UserEmail;
-	
-	
+
 	public String getUserEmail() {
 		return UserEmail;
 	}
-
 
 	public void setUserEmail(String userEmail) {
 		UserEmail = userEmail;
 	}
 
 	private String Password;
-	
-	
+
 	public String getUserName() {
 		return UserName;
 	}
-
 
 	public void setUserName(String userName) {
 		UserName = userName;
 	}
 
-
-
 	public String getUserID() {
 		return UserID;
 	}
-
-
 
 	public void setUserID(String userID) {
 		UserID = userID;
 	}
 
-
-
 	public String getPassword() {
 		return Password;
 	}
-
-
 
 	public void setPassword(String password) {
 		Password = password;
 	}
 
+	public void update(User newUser) {
+		// TODO Auto-generated method stub
+		
+		this.Password = newUser.Password;
+		this.UserEmail = newUser.UserEmail;
+		this.UserName = newUser.UserName;
+		this.UserID = newUser.UserID;
+		System.out.println("update" +newUser);
+	}
 
-
-
-	
-
-	
 	@Override
 	public String toString() {
-		return "User [UserName=" + UserName + ", UserID=" + UserID+", UserEmail=" + UserEmail + ", Password=" + Password + "]";
+		return "User [UserName=" + UserName + ", UserID=" + UserID + ", UserEmail=" + UserEmail + ", Password="
+				+ Password + "]";
 	}
 }
--- src/main/java/com/hello/web/UserController.java
+++ src/main/java/com/hello/web/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.hello.domain.User;
 import com.hello.domain.UserReposity;
@@ -14,38 +15,54 @@ import com.hello.domain.UserReposity;
 @Controller
 @RequestMapping("/users")
 public class UserController {
-	
+
 	@Autowired
 	private UserReposity userReposity;
-	
-	
+
 	@GetMapping("/form")
 	public String form() {
+		System.out.println("@GetMapping(\"/form\")");
 		return "user/form";
 	}
-	
+
 	@PostMapping("")
-	public String create(User user)
-	{
-		System.out.println("postmapping  "+user);
+	public String create(User user) {
+		System.out.println("@PostMapping(\"\")");
+		System.out.println("postmapping  " + user);
 		userReposity.save(user);
-		return "redirect:users";
+		return "redirect:/users";
 	}
-	
+
 	@GetMapping("")
-	public String list(Model model)
-	{
-		System.out.println("model: "+ model);
+	public String list(Model model) {
+		System.out.println("@GetMapping(\"\")");
+		System.out.println("model: " + model);
 		model.addAttribute("users", userReposity.findAll());
 		return "user/list";
 	}
 	
+//	@GetMapping("/users")
+//	public String list2(Model model) {
+//		System.out.println("model: " + model);
+//		model.addAttribute("users", userReposity.findAll());
+//		return "user/list";
+//	}
+
 	@GetMapping("/{id}/form")
 	public String updateForm(@PathVariable Long id, Model model) {
+		System.out.println("@GetMapping(\"/{id}/form\")");
 		User user = userReposity.findById(id).get();
 		model.addAttribute("user", user);
 		return "user/updateForm";
 	}
-	
 
+	
+	@PostMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		System.out.println("@PostMapping(\"/{id}\")");
+		User user = userReposity.findById(id).get();
+		user.update(newUser);
+		userReposity.save(user);
+		return "redirect:/users";
+	}
 }
